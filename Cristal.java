c68319d6d79f108de311e8b7735a676f9c89d1f2
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-06-17 15:22:56 +0200

    Add files via upload

--- Cristal.java
+++ Cristal.java
@@ -1,14 +1,17 @@
-public class Cristal extends caseHexa
-{
-	private int    valeur;
+public class Cristal extends caseHexa {
+	private int valeur;
 	private String couleur;
 
-	public Cristal(int valeur, String couleur)
-	{
-		this.valeur  = valeur;
+	public Cristal(int valeur, String couleur) {
+		this.valeur = valeur;
 		this.couleur = couleur;
 	}
 
-	public int    getValeur()  { return this.valeur; }
-	public String getCouleur() { return this.couleur; }
+	public int getValeur() {
+		return this.valeur;
+	}
+
+	public String getCouleur() {
+		return this.couleur;
+	}
 }
