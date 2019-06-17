64a05ff76edb37a0cf52a57442f68ee285b98523
dylandprs <43884685+dylandprs@users.noreply.github.com>
2019-06-17 15:10:39 +0200

    Add files via upload

---
+++ Cristal.java
@@ -0,0 +1,14 @@
+public class Cristal extends caseHexa
+{
+	private int    valeur;
+	private String couleur;
+
+	public Cristal(int valeur, String couleur)
+	{
+		this.valeur  = valeur;
+		this.couleur = couleur;
+	}
+
+	public int    getValeur()  { return this.valeur; }
+	public String getCouleur() { return this.couleur; }
+}
