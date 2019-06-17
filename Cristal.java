a1fa3ddb97699b49cd05bb2cab8f0040c3557dab
AisaTemp <45934456+AisaTemp@users.noreply.github.com>
2019-06-17 22:23:58 +0900

    edede

--- Cristal.java
+++ Cristal.java
@@ -1,14 +1,12 @@
-public class Cristal extends caseHexa
-{
+public class Cristal extends caseHexa {
 	private int    valeur;
 	private String couleur;
 
-	public Cristal(int valeur, String couleur)
-	{
+	public Cristal(int valeur, String couleur) {
 		this.valeur  = valeur;
 		this.couleur = couleur;
 	}
 
-	public int    getValeur()  { return this.valeur; }
+	public int    getValeur()  { return this.valeur;  }
 	public String getCouleur() { return this.couleur; }
 }
---
+++ Joueur.java
@@ -0,0 +1,9 @@
+public class Joueur {
+
+	private String nom;
+	private int points;
+	private boolean hasModifieProg;
+	private Robot[] tabRobot;
+
+	public Joueur(String nom)
+}
