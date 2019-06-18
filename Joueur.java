8f1f0efc92e3a1f93695af1165bfc2bd2c11feae
AisaTemp <45934456+AisaTemp@users.noreply.github.com>
2019-06-18 22:39:19 +0900

    Joueur et Robot

--- Joueur.java
+++ Joueur.java
@@ -2,25 +2,94 @@ import java.util.ArrayList;
 
 public class Joueur {
 
-	private static String couleurs = {"ROUGE", "JAUNE", "VERT",
-	                                  "BLEU", "VIOLET", "ROSE"};
+	private static String couleurs     = { "ROUGE", "JAUNE", "VERT",
+	                                       "BLEU", "VIOLET", "ROSE"};
+
+	private static String ordresString = { "ZAP", "AVANCER X1", "AVANCER X2" ,
+                                             "TOURNER GAUCHE", "TOURNER DROITE",
+						         "CHARGER", "DECHARGER", "DOUBLE"  };
 
 	private String nom;
 	private int points;
 	private boolean hasModifieProg;
+	private int[] etatOrdres;
 	private ArrayList<Robot> alRobot;
 
-	public Joueur(String nom) {
-		this.nom = nom;
+	public Joueur(int id) {
+		//this.nom = nom;
 
 		this.points = 0;
 		this.hasModifieProg = false;
 		this.alRobot = new ArrayList<Robot>();
+
+		// Les ID du tableau ordresString correspond aux ID du tableau
+		// etatOrdres. Il a y donc 2 ZAP, 2 AVANCER X1 etc...
+		this.etatOrdres = {2,2,1,3,3,2,2,1};
+
 	}
 
-	public void permutterOrdre(int idRobot, int ordre1, int ordre2) {
+	/**
+	  * Phase d'ajout de l'ordre. On retire un ordre et le place sur le robot
+	  * si possible. Si un ordre était déjà présent à l'emplacement indiqué,
+	  * on redonne la tuile ordre qui était initialement implémentée sur le
+	  * robot au joueur. Retourne true
+	  * Sinon, retourne false;
+	  */
 
+	public boolean ajouterOrdre(int idRobot, int emplacementOrdre, int idOrdre) {
+		// Phase d'ajout de l'ordre
+		if(this.etatOrdres[emplacementOrdre] <= 0) {
+			return false;
+		}
+		else {
+			this.etatOrdres[emplacementOrdre]--;
+			String ordreRetour = this.alRobot.get(idRobot).
+			                          setOrdre(Joueur.ordresString[emplacementOrdre]);
+
+			// On remet la tuile ordre au joueur s'il y en avait une
+			if(ordreRetour != null) {
+				this.etatOrdres[getEmplacementOrdre(ordreRetour)]++;
+			}
+			return true;
+		}
 	}
+
+	/**
+	  * Permutte deux tuiles d'ordre sur un robot s'il en existe au moins
+	  * une sur les deux positions données. Retoune true
+	  * Sinon, return false
+	  */
+	public boolean permutterOrdre(int idRobot, int idOrdre1, int idOrdre2) {
+		String ordre1Temp = this.alRobot.get(idRobot).getOrdre(idOrdre1);
+		String ordre2Temp = this.alRobot.get(idRobot).getOrdre(idOrdre2);
+		if(ordre1Temp != null || ordre2Temp != null) {
+			this.alRobot.get(idRobot).setOrdre(idOrdre1, ordre2Temp);
+			this.alRobot.get(idRobot).setOrdre(idOrdre2, ordre1Temp);
+			return true;
+		}
+		else
+			return false;
+	}
+
+	/**
+	  * Active la méthode retirerOrdre de Robot, et récupère un String
+	  * Si celui-ci n'est pas null, il augmente son nombre d'ordres correspondant
+	  * Retourne true
+	  * Sinon, ne fait rien et retourne false
+	  */
+	public boolean retirerOrdre(int idRobot, int idOrdre) {
+		String ordreTemp = this.alRobot.get(idRobot).retirerOrdre(idOrdre);
+		if(ordreTemp != null) {
+			this.etatOrdres[getEmplacementOrdre(ordreTemp)]++;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean redemarrer(int idRobot) {
+		return this.alRobot.get(idRobot).redemarrer();
+	}
+
 
 	// Méthode appelée une seule fois, lors de l'initialisation
 	// de la partie.
@@ -38,5 +107,17 @@ public class Joueur {
 
 	public int getPoints() {
 		return this.points;
+	}
+
+	/**
+	  * Méthode afin de retrouver l'emplacement sous forme de int
+	  * d'un ordre lorsque l'on connait son nom
+	  */
+	private int getEmplacementOrdre(String nomOrdre) {
+		for(int i = 0; i < Joueur.ordresString.length; i++) {
+			if(Joueur.ordresString[i].equals(nomOrdre))
+				return i;
+		}
+		return -1;
 	}
 }
--- Robot.java
+++ Robot.java
@@ -1,9 +1,7 @@
 public class Robot extends CaseHexa {
-	
-	private String[] instructions;
 
 	private int dir;
-
+	private String[] ordres;
 	private Cristal cristal;
 
 	public Robot(int posX, int posY, int dir) {
@@ -12,13 +10,64 @@ public class Robot extends CaseHexa {
 		this.poussable = true;
 
 		this.cristal = null;
-		this.instructions = null;
+		this.ordres = null;
 	}
 
 	public void avancer(int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
 	}
+
+	public void tourner(int sens) {
+		// %6 car modulo de Case hexagonale
+		this.dir = (this.dir + sens) % 6;
+	}
+
+	public void setOrdres(String[] ordres) {
+		this.ordres = ordres;
+	}
+
+	// Retourne un String ordre au cas où on permutte deux ordres
+	public void setOrdre(int idOrdre, String ordreString) {
+		String ancienOrdre = this.ordres[idOrdre];
+		this.ordres[idOrdre] = ordreString;
+		return ancienOrdre;
+	}
+
+	/**
+	  * Retire et renvoi l'ordre de l'emplacement s'il n'est pas null,
+	  * et le place à null
+	  * Renvoie null sinon
+	  */
+	public String retirerOrdre(int idOrdre) {
+		if(this.ordres[idOrdre] != null) {
+			String retour = this.ordres[idOrdre];
+			this.ordres[idOrdre] = null;
+			return retour;
+		}
+		else
+			return null;
+	}
+
+	public boolean redemarrer() {
+		for(String o : ordres) {
+			o = null;
+		}
+		return true;
+	}
+
+	public void charger(Cristal cristal) {
+		this.cristal = cristal;
+	}
+
+	public void decharger() {
+		this.cristal = null;
+	}
+
+
+	/***********************/
+	/*      Accesseurs     */
+	/***********************/
 
 	public int getPosX() {
 		return this.posX;
@@ -28,33 +77,20 @@ public class Robot extends CaseHexa {
 		return this.posY;
 	}
 
-	public void tourner(int sens) {
-		// %6 car modulo de Case hexagonale
-		this.dir = (this.dir + sens) % 6;
-	}
-
 	public int getDir() {
 		return this.dir;
 	}
 
-	public String[] getInstrution() {
-		return this.instructions;
+	public String[] getOrdres() {
+		return this.ordres;
 	}
 
-	public void setInstruction(String[] instruction) {
-		this.instructions = instructions;
-	}
-
-	public void charger(Cristal cristal) {
-		this.cristal = cristal;
+	public String getOrdre(int idOrdre) {
+		return this.ordres[idOrdre];
 	}
 
 	public Cristal getCristal() {
 		return this.cristal;
-	}
-
-	public void decharger() {
-		this.cristal = null;
 	}
 
 	public String toString() {
