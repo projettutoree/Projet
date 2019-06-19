675f3acf413e17aa6c8cc4de8de85c6008c21ff1
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-06-19 10:33:35 +0200

    Add files via upload

--- Ordre.java
+++ Ordre.java
@@ -1,40 +1,17 @@
 import java.util.ArrayList;
 
-public class Ordre {
-
+public abstract class Ordre {
 	private static ArrayList<CaseHexa> alCase;
+	private static Robot robot;
 
-	public static void executerProgramme(ArrayList<CaseHexa> alCase, ArrayList<Robot> alRobot) {
+	public static void executer(Robot robot) {
+		Ordre.robot = robot;
+		action();
+	}
 
-		Ordre.alCase = alCase;
+	public static void action() {}
 
-		for (Robot r : alRobot) {
-			for (String ordre : r.getOrdres()) {
-				switch (ordre.toUpperCase()) {
-				case "AVANCER X1":
-					Ordre.avancer(r);
-					break;
-				case "AVANCER X2":
-					Ordre.avancer(r);
-					Ordre.avancer(r);
-					break;
-				case "TOURNER SENS ANTI HORAIRE":
-					r.tourner(-1);
-					break;
-				case "TOURNER SENS HORAIRE":
-					r.tourner(1);
-					break;
-				case "CHARGER":
-					Ordre.charger(r);
-					break;
-				case "DEPOSER":
-					Ordre.deposer(r);
-					break;
-				case "ZAP":
-					Ordre.zap(r);
-					break;
-				}
-			}
-		}
+	public static setCase(ArrayList<CaseHexa> alCase){
+		this.alCase = alCase;
 	}
 }
